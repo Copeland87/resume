8e90a84aacf2b6971e2c164be811a44aceb0841b
unknown <zcopeland87@gmail.com>
2017-09-13 03:01:02 -0600

    update

--- assets/js.js
+++
@@ -1,8 +0,0 @@
-var para = document.querySelector('p')
-
-para.addEventListener('click', updateName);
-
-function updateName() {
-	var name = prompt('Enter Your Name');
-	para.textContent = 'Welcome, ' + name;
-}
